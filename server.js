7df90c676cd64ff1d448d65f906eb3bee09dcea4
Kai Bizik <code@kai.bizik.de>
2018-12-16 22:17:27 +0100

    Add errorhandler, refactoring

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
+const apiRouter = require('./api/api.js');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const errorhandler = require('errorhandler');
 const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
+module.exports = app;
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('combined'));
 
-// TODO: routes go here
+if (process.env.NODE_ENV === 'development') {
+	app.use(errorhandler());
+}
 
-const port = process.env.PORT || 4001;
+app.use('/api', apiRouter);
+
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
 	console.log(`Expresso running on port ${port}`);
 });
